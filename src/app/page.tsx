80dee801ffaf93fc111e11a000fb9da1072822ce
Nirnay Behera <nirnay.1behera@gmail.com>
2023-08-23 21:21:06 +0530

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,49 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="font-bold w-1/2">
-        <h2>Login via Spotify to use</h2>
-        <Button title="Login" />
-      </div>
-    </main>
+    <>
+      <main className="flex min-h-screen items-center justify-between">
+        <div
+          className="flex flex-col justify-center align-middle h-screen w-full md:w-1/2 p-24"
+          style={{ backgroundColor: "#191414", color: "white" }}
+        >
+          <div className="text-center">
+            <h1 className="text-5xl md:text-5xl lg:text-5xl py-1 font-lora">
+              Elevate Your Music Mood
+            </h1>
+            <p className="text-2xl md:2xl lg:2xl py-2 font-thin">
+              Custom Playlists, Artist & Genre Options, and Enriched Spotify
+              Profile View
+            </p>
+            <Button
+              title="Login"
+              style={{
+                fontSize: "1.25rem",
+                borderRadius: "30px",
+                padding: "10px 30px",
+              }}
+            />
+          </div>
+        </div>
+        <div
+          className="w-1/2 relative"
+          style={{
+            backgroundImage: `url("/images/bg3.jpg")`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionX: "-100px",
+            minHeight: "100vh",
+          }}
+        >
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundColor: "#1DB954",
+              opacity: "0.3",
+            }}
+          />
+        </div>
+      </main>
+    </>
   );
 }
